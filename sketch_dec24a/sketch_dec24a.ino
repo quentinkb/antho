3029691bea9b28e7114626fd794b8511e59d7a65
Quentin BERNET <q.bernet@gmail.com>
2017-12-24 12:15:36 +0100

    set up pins
    set up des pins (input,output)

--- sketch_dec24a/sketch_dec24a.ino
+++ sketch_dec24a/sketch_dec24a.ino
@@ -2,6 +2,19 @@
 
 void setup() {
   // put your setup code here, to run once:
+  int i ; 
+  int nb_led_horizontal = sizeof(LED_HORIZONTAL) ; 
+  for (i = 0; i < nb_led_horizontal ; i++) {
+    pinMode(LED_HORIZONTAL[i], OUTPUT) ; 
+  }
+
+  int nb_led_vertical = sizeof(LED_VERTICAL) ; 
+  for (i = 0; i < nb_led_vertical ; i++) {
+    pinMode(LED_VERTICAL[i], OUTPUT) ; 
+  }
+
+  pinMode(PWM_HORIZONTAL, INPUT) ; 
+  pinMode(PWM_VERTICAL, INPUT) ; 
 
 }
 
