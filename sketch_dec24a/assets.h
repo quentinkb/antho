04480f1775913fd8271e7e4dc6c396e03fad3ddc
Quentin BERNET <q.bernet@gmail.com>
2017-12-24 12:07:02 +0100

    add rates values
    ajout des valeurs de seuils

--- sketch_dec24a/assets.h
+++ sketch_dec24a/assets.h
@@ -2,6 +2,9 @@ const int PWM_HORIZONTAL ;
 const int PWM_VERTICAL ; 
 
 const int LED_HORIZONTAL[6] =  {1,2,3,4,5,6}  ; 
-const int LED_VERTIAL[6] = {7,8,9,10,11,12} ; 
+const int LED_VERTICAL[6] = {7,8,9,10,11,12} ; 
 
+const int RATE_HORIZONTAL[3] = {1,2,3} ; 
+const int RATE_VERTICAL[3] = {1,2,3} ; 
 
+ 
