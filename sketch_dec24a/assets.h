280d1e4690c1c100ac1301a4791b9a30ba621b5d
Quentin BERNET <q.bernet@gmail.com>
2017-12-24 18:37:33 +0100

    change value
    change value

--- sketch_dec24a/assets.h
+++ sketch_dec24a/assets.h
@@ -3,36 +3,36 @@
  * @desc valeur du pwm horizontal
  * @type int 
  */
-const int PWM_HORIZONTAL = 1 ;
+const int PWM_HORIZONTAL = A0 ; 
 /**
  * @var PWM_VERTICAL
  * @desc valeur du pwm vertical
  * @type int 
  */ 
-const int PWM_VERTICAL = 2  ; 
+const int PWM_VERTICAL = A1  ; 
 /**
  * @var LED_HORIZONTAL
  * @desc tableau des leds pour la direction horizontale 
  * @type array(int) 
  */
-const int LED_HORIZONTAL[6] =  {1,2,3,4,5,6}  ; 
+const int LED_HORIZONTAL[6] =  {3,4,5,6,7,8}  ; 
 /**
  * @var LED_VERTICAL
  * @desc tableau des leds pour la direction verticale 
  * @type array(int) 
  */
-const int LED_VERTICAL[6] = {7,8,9,10,11,12} ; 
+const int LED_VERTICAL[6] = {9,10,11,12,13,2} ; 
 /**
  * @var RATE_HORIZONTAL
  * @desc tableau des seuils pour la direction horizontale 
  * @type array(int) 
  */
-const int RATE_HORIZONTAL[6] = {1,2,3,4,5,6} ; 
+const int RATE_HORIZONTAL[6] = {1000,1200,1400,1600,1800,2000} ; 
 /**
  * @var RATE_VERTICAL
  * @desc tableau des leds pour la direction verticale 
  * @type array(int) 
  */
-const int RATE_VERTICAL[6] = {1,2,3,4,5,6} ; 
+const int RATE_VERTICAL[6] = {1000,1200,1400,1600,1800,2000} ; 
 
  
