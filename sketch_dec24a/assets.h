11f78f52fedf6cd96d5a0acc0fe0ab970f1f2030
Quentin BERNET <q.bernet@gmail.com>
2017-12-24 12:26:37 +0100

    logique pour horizontal left
    logique pour horizontal left

--- sketch_dec24a/assets.h
+++ sketch_dec24a/assets.h
@@ -1,10 +1,10 @@
-const int PWM_HORIZONTAL ; 
-const int PWM_VERTICAL ; 
+const int PWM_HORIZONTAL = 1 ; 
+const int PWM_VERTICAL = 2  ; 
 
 const int LED_HORIZONTAL[6] =  {1,2,3,4,5,6}  ; 
 const int LED_VERTICAL[6] = {7,8,9,10,11,12} ; 
 
-const int RATE_HORIZONTAL[3] = {1,2,3} ; 
-const int RATE_VERTICAL[3] = {1,2,3} ; 
+const int RATE_HORIZONTAL[6] = {1,2,3,4,5,6} ; 
+const int RATE_VERTICAL[6] = {1,2,3,4,5,6} ; 
 
  
--- sketch_dec24a/sketch_dec24a.ino
+++ sketch_dec24a/sketch_dec24a.ino
@@ -3,12 +3,12 @@
 void setup() {
   // put your setup code here, to run once:
   int i ; 
-  int nb_led_horizontal = sizeof(LED_HORIZONTAL) ; 
+  int nb_led_horizontal = sizeof(LED_HORIZONTAL)/sizeof(int) ; 
   for (i = 0; i < nb_led_horizontal ; i++) {
     pinMode(LED_HORIZONTAL[i], OUTPUT) ; 
   }
 
-  int nb_led_vertical = sizeof(LED_VERTICAL) ; 
+  int nb_led_vertical = sizeof(LED_VERTICAL)/sizeof(int) ; 
   for (i = 0; i < nb_led_vertical ; i++) {
     pinMode(LED_VERTICAL[i], OUTPUT) ; 
   }
@@ -20,5 +20,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int horizontal_value = analogRead(PWM_HORIZONTAL) ; 
+  int vertical_value = analogRead(PWM_VERTICAL); 
+
+  // horizontal treatment left 
+  int nb_rates = sizeof(RATE_HORIZONTAL)/sizeof(int); 
+  int nb_led_horizontal = sizeof(LED_HORIZONTAL)/sizeof(int);
+  if (nb_rates == nb_led_horizontal) {
+    int j ; 
+    for(j = 0 ; j < nb_rates -3 ; j++) {
+       if (horizontal_value < RATE_HORIZONTAL[j]) {
+          digitalWrite(LED_HORIZONTAL[j],HIGH) ; 
+       } else {
+          digitalWrite(LED_HORIZONTAL[j],LOW) ; 
+       }
+    }
+  }
 
 }
